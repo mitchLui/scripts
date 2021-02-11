81d14322cf50c0c459fb583b82fddedc87a3b011
Mitch Lui <katinlui@gmail.com>
2021-02-11 15:26:33 +0000

    Added 3rd party app downloads

--- setup.sh
+++ setup.sh
@@ -143,6 +143,46 @@ MAC_IDS=(
 
 mas install ${MAC_IDS[@]}
 
-#TODO DOWNLOAD 3rd PARTY APPS
+cd ~/Downloads
+
+APP_URLS=(
+    https://desktop-release.notion-static.com/Notion-2.0.11.dmg
+    https://download01.logi.com/web/ftp/pub/controldevices/unifying/unifying1.3.375_mac.zip
+    https://teams.microsoft.com/downloads/desktopurl?env=production&plat=osx&arch=&download=true
+    https://coderunnerapp.com/download
+    https://coderunner.nyc3.cdn.digitaloceanspaces.com/CodeRunner-4.0.3.zip
+    https://charliemonroesoftware.com/trial/downie/v4/Downie_4_4208.dmg
+    https://charliemonroesoftware.com/trial/permute/v3/Permute_3_2338.dmg
+    https://desktop.figma.com/mac/Figma.zip
+    https://dl.devmate.com/com.macpaw.CleanMyMac4/CleanMyMacX.dmg
+    https://downloads.imazing.com/mac/iMazing/iMazing2forMac.dmg
+    https://daisydiskapp.com/downloads/DaisyDisk.zip
+    https://s3.amazonaws.com/cindori/Sensei.dmg
+    https://github.com/qmk/qmk_toolbox/releases/download/0.0.21/QMK.Toolbox.app.zip
+    https://github.com/the-via/releases/releases/download/v1.3.1/via-1.3.1-mac.dmg
+    https://www.dropbox.com/s/m9c49ia10kx3g2b/BootMapperClient.mac.zip?dl=0
+    https://cachefly.alfredapp.com/Alfred_4.3.1_1214.dmg
+    https://enduranceapp.com/downloads/Endurance3.1.zip
+    https://v2.airbuddy.app/download
+    https://crystalidea.com/downloads/macsfancontrol.zip
+    https://d2oxtzozd38ts8.cloudfront.net/loopback/download/Loopback.zip
+    https://download01.logi.com/web/ftp/pub/techsupport/cameras/Webcams/LogiCameraSettings_3.0.23.pkg
+    https://downloads.caldigit.com/CalDigit_TS3_Plus_Firmware_Updater.zip
+    https://downloads.caldigit.com/CalDigit-Docking-Station-Utility.zip
+    https://cdn.eltima.com/download/downloader_mac.dmg
+    https://coconut-flavour.com/downloads/coconutBattery_latest.zip
+    https://d1k3ac1niusr4e.cloudfront.net/soulver.zip
+    https://download01.logi.com/web/ftp/pub/techsupport/options/Options_8.36.76.zip
+    https://download.parallels.com/desktop/v16/16.1.3-49160/ParallelsDesktop-16.1.3-49160.dmg
+    https://github.com/glouel/AerialCompanion/releases/latest/download/AerialCompanion.dmg
+    https://www.macbartender.com/B2/updates/B4Latest/Bartender%204.dmg
+    https://zoom.us/client/latest/Zoom.pkg
+    https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v3.12.1/DB.Browser.for.SQLite-3.12.1-v2.dmg
+    https://github.com/DigiDNA/Silicon/releases/download/1.0.3/Silicon.app.zip
+    https://devimages-cdn.apple.com/design/resources/download/SF-Symbols-2.1.dmg
+    https://d2oxtzozd38ts8.cloudfront.net/audiohijack/download/AudioHijack.zip
+)
+
+wget ${APP_URLS[@]}
 
 #TODO SETUP SYSTEM PREFERENCES
