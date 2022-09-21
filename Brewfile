018e248b3cc1d6ab4a7f642cac2529ce39f8de4c
Mitch Lui <mitch@mitchlui.dev>
2022-09-21 15:37:03 +0100

    brew bundle and some other shit

---
+++ Brewfile
@@ -0,0 +1,114 @@
+tap "bell-sw/liberica"
+tap "heroku/brew"
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/cask-drivers"
+tap "homebrew/core"
+tap "jandedobbeleer/oh-my-posh"
+tap "microsoft/git"
+tap "osx-cross/arm"
+tap "osx-cross/avr"
+tap "qmk/qmk"
+brew "act"
+brew "webp"
+brew "awscli"
+brew "bash"
+brew "bash-completion"
+brew "gettext"
+brew "glib"
+brew "gnutls"
+brew "cask"
+brew "cmatrix"
+brew "docui"
+brew "ethereum"
+brew "harfbuzz"
+brew "sdl2"
+brew "ffmpeg"
+brew "fish"
+brew "gcc"
+brew "git", link: false
+brew "go"
+brew "graphviz"
+brew "htop"
+brew "kind"
+brew "kompose"
+brew "kubernetes-cli"
+brew "mas"
+brew "openjdk"
+brew "maven"
+brew "mysql"
+brew "neofetch"
+brew "node"
+brew "nvm"
+brew "openjdk@11"
+brew "parallel"
+brew "poppler"
+brew "pre-commit"
+brew "pyenv"
+brew "pyenv-pip-migrate"
+brew "rbenv"
+brew "tmux"
+brew "transmission-cli"
+brew "tree"
+brew "vim"
+brew "wget"
+brew "yq"
+brew "heroku/brew/heroku"
+brew "qmk/qmk/qmk"
+cask "airbuddy"
+cask "airfoil"
+cask "audio-hijack"
+cask "caldigit-docking-utility"
+cask "git-credential-manager-core"
+cask "liberica-jdk15-full"
+cask "logitech-camera-settings"
+cask "logitech-options"
+cask "logitech-unifying"
+cask "microsoft-auto-update"
+cask "microsoft-office"
+cask "microsoft-teams"
+cask "minecraft"
+cask "ngrok"
+cask "sizzy"
+cask "steam"
+cask "warp"
+cask "zoom"
+mas "AdBlock Pro", id: 1018301773
+mas "Affinity Designer", id: 824171161
+mas "Apple Configurator", id: 1037126344
+mas "Balance Lock", id: 1019371109
+mas "Cardhop", id: 1290358394
+mas "Codye", id: 1516894961
+mas "Dark Noise", id: 1465439395
+mas "Developer", id: 640199958
+mas "Diagrams", id: 1276248849
+mas "Drafts", id: 1435957248
+mas "F5Access", id: 1243219105
+mas "Final Cut Pro", id: 424389933
+mas "GoodNotes", id: 1444383602
+mas "HTTPBot", id: 1232603544
+mas "Keynote", id: 409183694
+mas "Logoist 4", id: 1493005285
+mas "Mactracker", id: 430255202
+mas "Magnet", id: 441258766
+mas "Markdown Editor Pro", id: 1453295546
+mas "Movist", id: 461788075
+mas "Noir", id: 1592917505
+mas "Numbers", id: 409203825
+mas "Pages", id: 409201541
+mas "Parcel", id: 639968404
+mas "PDF Viewer", id: 1120099014
+mas "Pixelmator Pro", id: 1289583905
+mas "Prime Video", id: 545519333
+mas "Screens", id: 1224268771
+mas "Spark", id: 1176895641
+mas "Step Two", id: 1448916662
+mas "Telegram", id: 747648890
+mas "TestFlight", id: 899247664
+mas "Textastic", id: 572491815
+mas "The Unarchiver", id: 425424353
+mas "Trello", id: 1278508951
+mas "Twitter", id: 1482454543
+mas "Unclutter", id: 577085396
+mas "WhatsApp", id: 1147396723
+mas "Xcode", id: 497799835
--- setup.sh
+++ setup.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-echo "Switching to bash..."
-
-chsh -s /bin/bash
-
 echo "Setting up machine $HOSTNAME..."
 
 echo "Installing Rosetta 2..."
@@ -18,121 +14,14 @@ echo "Installing homebrew..."
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-echo "Upgrading bash..."
-
-brew install bash
-
-echo "Tapping into taps..."
-
-brew tap homebrew/cask-drivers
-brew tap jandedobbeleer/oh-my-posh
-brew tap ethereum/ethereum
-brew tap qmk/qmk
-
-echo "Installing packages..."
-
-PACKAGES=(
-    cask
-    golang
-    openjdk
-    node
-    tree
-    wget
-    htop
-    neofetch
-    docker
-    kubectl
-    minikube
-    kind
-    bash-completion
-    ffmpeg
-    vim
-    git
-    docui
-    pyenv
-    mysql
-    cabal-install
-    mas
-    tmux
-    micro
-    mvn
-    pypy
-    pyenv-pip-migrate
-    qmk
-    kompose
-    ethereum
-    solidity
-    fish
-    oh-my-posh
-)
-
-brew install ${PACKAGES[@]}
+brew bundle
 
 echo "Installing Python..."
 
-PYTHON_VER="3.9.7"
+PYTHON_VER="3.10.6"
 
 pyenv install $PYTHON_VER
 pyenv global $PYTHON_VER
-
-echo "Installing casks..."
-
-CASKS=(
-    ngrok
-    vagrant
-    caldigit-docking-utility
-    logitech-options
-    logitech-unifying
-    aerial
-    airbuddy
-    airfoil
-    alfred
-    audio-hijack
-    logitech-camera-settings
-    cleanmymac
-    coconutbattery
-    daisydisk
-    db-browser-for-sqlite
-    discord
-    docker
-    downie
-    endurance
-    figma
-    firefox
-    folx
-    imazing
-    ledger-live
-    loopback
-    macs-fan-control
-    microsoft-office
-    microsoft-teams
-    minecraft
-    minecraft-server
-    mysqlworkbench
-    parallels
-    permute
-    postman
-    qmk-toolbox
-    safari-technology-preview
-    sensei
-    sf-symbols
-    soulver
-    soundsource
-    steam
-    terminus
-    via
-    virtualbox
-    visual-studio-code
-    wifi-explorer
-    zoom
-    etrecheckpro
-    binance
-    tabby
-)
-
-brew install --cask ${CASKS[@]}
-
-echo `"Cleaning up..."`
 
 brew cleanup
 
@@ -143,7 +32,6 @@ PIP_PACKAGES=(
     requests
     flask
     flask_cors
-    fbmessenger
     six
     gspread
     oauth2client
@@ -169,52 +57,6 @@ PIP_PACKAGES=(
 
 pip3 install ${PIP_PACKAGES[@]}
 
-MAC_IDS=(
-    497799835  #Xcode
-    1278508951 #Trello
-    430255202  #MacTracker
-    572491815  #Textastic
-    1493005285 #Logoist
-    1435957248 #Drafts
-    1449412482 #Reeder
-    682658836  #Garageband
-    1116599239 #NordVPN
-    1276248849 #Diagrams
-    1290358394 #Cardhop
-    1147396723 #WhatsApp
-    424389933  #Final Cut Pro
-    577085396  #Unclutter
-    993487541  #CARROT
-    1019371109 #Balance Lock
-    1224268771 #Screens
-    1037126344 #Apple Configurator
-    1289583905 #Pixelmator Pro
-    1176895641 #Spark
-    441258766  #Magnet
-    425424353  #Unarchiver
-    1482454543 #Twitter
-    1478447657 #Geekbench
-    409201541  #Pages
-    1444383602 #GoodNotes
-    747648890  #Telegram
-    1120099014 #PDF Viewer
-    640199958  #Developer
-    639968404  #Parcel
-    409203825  #Numbers
-    461788075  #Movist
-    409183694  #Keynote
-    1018301773 #AdBlock Pro
-    1516894961 #Codye
-    1453295546 #Markdown Editor Pro
-    1465439395 #Dark Noise
-    1232603544 #HTTPBot
-    1243219105 #F5 Access
-)
-
-mas install ${MAC_IDS[@]}
-
-npm install -g @vue/cli
-
 echo "Setting up ghc..."
 
 export BOOTSTRAP_HASKELL_NONINTERACTIVE=1
@@ -228,69 +70,10 @@ cd
 
 mkdir Local\ Documents
 
-cat > .bash_profile <<- EOM
-source ~/.bash_prompt
-source ~/.aliases
-
-export BASH_SILENCE_DEPRECATION_WARNING=1
-export PATH="~/.local/bin"
-export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
-export PATH="/usr/local/opt/openjdk@11/bin:$PATH"
-export NODE_ENV=development
-eval "$(/opt/homebrew/bin/brew shellenv)"
-export PYENV_ROOT="$HOME/.pyenv"
-
-export PATH="$PYENV_ROOT/bin:$PATH"
-export PATH="$PYENV_ROOT/shims:$PATH"
-if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init -)"
-fi
-[[ -f ~/.bashrc ]] && source ~/.bashrc # ghcup-env
-
-if [ -f $(brew --prefix)/etc/bash_completion ]; then
-  . $(brew --prefix)/etc/bash_completion
-fi
-if [ -f $(brew --prefix)/etc/bash_completion.d/git-completion.bash ]; then
-  . $(brew --prefix)/etc/bash_completion.d/git-completion.bash
-fi
-if [ -f `brew --prefix`/etc/bash_completion.d/git-flow-completion.bash ]; then
-  . `brew --prefix`/etc/bash_completion.d/git-flow-completion.bash
-fi
-eval "$(rbenv init - bash)"
-export PATH="/usr/local/opt/ruby/bin:/usr/local/lib/ruby/gems/3.0.0/bin:$PATH"
-
+cat > .zshrc <<- EOM
+PROMPT='%F{cyan}%n%f ~ '
 EOM
 
-cat > .bash_prompt <<- EOM
-#!/usr/bin/env bash
 
-export CLICOLOR=1
-export LSCOLORS=ExFxBxDxCxegedabagacad
-
-#TERMINAL PROMPT
-
-PS1="\[\e[01;92m\]\u\[\e[m\]" #username
-PS1+=" "
-PS1+="\[\e[0;95m\]\W\[\e[m\]" #current directory
-PS1+=" "
-PS1+="$ "
-
-export PS1;
-;
-EOM
-
-cat > .bashrc <<- EOM
-export BASH_SILENCE_DEPRECATION_WARNING=1
-eval "$(pyenv init -)"
-[ -f "/Users/mitchlui/.ghcup/env" ] && source "/Users/mitchlui/.ghcup/env" # ghcup-env
-
-export PATH="/Users/mitchlui/.deta/bin:$PATH"
-
-
-EOM
-
-. .bash_profile
-
-#TODO SETUP SYSTEM PREFERENCES
 
 
